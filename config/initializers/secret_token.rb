014f0d637af4a345e81049d104adc2645919b7ac
unknown <roger@.(none)>
2011-07-05 12:37:56 +0200

    Initial commit : - hobo config - User config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GatewayManagerHobo::Application.config.secret_token = '21683aa7c7d753f8ded092f527cdbb38f29f8486f2b35e8376df4065c50711b447a8bc92e38b92c087e1988ffe3e607b3e7183bb23f99796bc529cf8e6b80814'
