242fb776a374e644c371ee8663bae89559d097e8
Mastersioux <charleric.paccard@gmail.com>
2012-02-02 15:33:58 +0100

    grand test final pour la reprise des programmes en cas de redemmarage

--- doc/autostart_prod.bat
+++ doc/autostart_prod.bat
@@ -1,7 +1,10 @@
 @ECHO OFF
 :BEGIN
 CLS
+@ECHO WAIT 180 sec
+ping 127.0.0.1 -n 180 >nul
 @ECHO UN POUR LES GOUVERNER TOUS :)
+CALL C:\Ruby192\bin\setrbvars.bat
 START "Rails Console Production" "C:\web applications\projectone\doc\console_prod.bat"
 START "Rails Server Production" "C:\web applications\projectone\doc\serveur_prod.bat"
 START "Rails Jobs Production" "C:\web applications\projectone\doc\jobs_prod.bat"
